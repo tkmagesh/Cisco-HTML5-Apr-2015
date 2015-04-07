119406875cecd6e968a53268e8338b8571fb84ef
tkmagesh <tkmagesh77@gmail.com>
2015-04-07 10:18:43 +0530

    Communication using messages

--- Day-02/01-MultiThreading/WebWorkers/complexWork.js
+++ Day-02/01-MultiThreading/WebWorkers/complexWork.js
@@ -1,5 +1,14 @@
-for(var i=0; i<10000; i++)
+function doWork(){
+    for(var i=0; i<10000; i++)
     for(var j=0; j<10000; j++)
         for(var k=0; k<100; k++){
         }
-console.log("Work completed");
+}
+self.addEventListener("message", function(arg){
+    var dataFromMain = arg.data;
+    if (dataFromMain === "start"){
+        doWork();
+        self.postMessage("done");
+    }
+});
+
