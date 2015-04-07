b9af3b95e7efa148d5e06c2f6430a1bb069ebb92
tkmagesh <tkmagesh77@gmail.com>
2015-04-07 10:48:40 +0530

    Progress Update - solution

--- Day-02/01-MultiThreading/WebWorkers/complexWork.js
+++ Day-02/01-MultiThreading/WebWorkers/complexWork.js
@@ -1,14 +1,22 @@
 function doWork(){
-    for(var i=0; i<10000; i++)
-    for(var j=0; j<10000; j++)
-        for(var k=0; k<100; k++){
+    for(var i=0; i<10000; i++){
+        for(var j=0; j<10000; j++)
+            for(var k=0; k<100; k++){
+            }
+        var percentCompleted = (i / 10000) * 100;
+        if (i % 100 === 0){
+            self.postMessage({
+                type : "progress",
+                percentCompleted : percentCompleted
+            });
         }
+    }
 }
 self.addEventListener("message", function(arg){
     var dataFromMain = arg.data;
     if (dataFromMain === "start"){
         doWork();
-        self.postMessage("done");
+        self.postMessage({type : "done"});
     }
 });
 
